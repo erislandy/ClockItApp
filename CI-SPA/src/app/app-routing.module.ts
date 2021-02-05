f5af9bd548648568194e532d9661db685811d2bc
erislandy <https.proxy>
2021-02-05 01:42:32 -0500

    Video 9 | Make Http request get values from api

--- CI-SPA/src/app/app-routing.module.ts
+++ CI-SPA/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ValueComponent } from './value/value.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ValueComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
